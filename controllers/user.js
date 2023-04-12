23eaa765a16619bbb81dad9910b00890797e60f2
Ashish Gupta <gashish4950@gmail.com>
2023-04-12 15:34:14 +0530

    fix-route

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
-
 import { sendCookies } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
 
@@ -34,13 +33,14 @@ export const register = async (req, res, next) => {
     if (user) return next(new ErrorHandler("User Already Exist", 400));
 
     const hasedPassword = await bcrypt.hash(password, 10);
+
     user = await User.create({
       name,
       email,
       password: hasedPassword,
     });
 
-    sendCookies(user, res, `Register Successfully ${user.name}`, 201);
+    sendCookies(user, res, `Register Successfully`, 201);
   } catch (error) {
     next(error);
   }
@@ -56,13 +56,12 @@ export const logout = (req, res) => {
   res
     .status(200)
     .cookie("token", "", {
-      httpOnly: true,
       expires: new Date(Date.now()),
       sameSite: process.env.NODE_ENV === "Developemnt" ? "lax" : "none",
       secure: process.env.NODE_ENV === "Developemnt" ? false : true,
     })
     .json({
-      sucess: true,
-      message: "Logout Succesfully",
+      success: true,
+      user: req.user,
     });
 };
--- utils/features.js
+++ utils/features.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const sendCookies = (user, res, message, statusCode = 200) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
   res
     .status(statusCode)
     .cookie("token", token, {
